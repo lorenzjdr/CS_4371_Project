7e8156000c92998b183d2dc5b1057df135898c46
lorenzjdr <lorenz.jdr@gmail.com>
2026-03-25 14:48:17 -0500

    Added goal for training the model

--- train_model.py
+++ train_model.py
@@ -1,3 +1,13 @@
+'''
+Train model on normal device behavior, then identify when devices act abnormally
+
+Loads normal network traffic data (environment OR patient monitoring devices)
+Extracts features from network packets (TCP flags, frame sizes, MQTT topics, etc.)
+Converts categorical features to numbers (encoding)
+Train an Isolation Forest model to learn what "normal" looks like
+Save the trained model to a .pkl file for use
+'''
+
 def load_data():
     #should load the csv file and clean it here
     pass
