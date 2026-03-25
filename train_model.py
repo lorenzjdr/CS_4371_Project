212ec1b6664013d6956335cf82a80e3f16d26e57
lorenzjdr <lorenz.jdr@gmail.com>
2026-03-25 13:27:33 -0500

    Added general outline for teammates

---
+++ train_model.py
@@ -0,0 +1,21 @@
+def load_data():
+    #should load the csv file and clean it here
+    pass
+
+def prepare_features():
+    #split the data into features and targets
+    pass
+
+def train_models():
+    # train selected models
+    pass
+
+def save_models():
+    # save models 
+    pass 
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
