6dfc61de36c0174324e9e068a31c4dbd4e60689b
Olivia Laurel <OLaurel113@gmail.com>
2026-04-04 14:34:01 -0500

    fixing bugs, remove nonnum cols, split feature/labels

--- Random_Forest/randomforest.py
+++ Random_Forest/randomforest.py
@@ -1,5 +1,8 @@
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report
 
 #Loading Dataset
 dataset = pd.read_csv("../Dataset/environmentMonitoring.csv")
@@ -8,13 +11,8 @@ print(dataset.head())
 print(f"Shape: {dataset.shape}")
 
 #Encoding string columns to nums so Random forest can read them
-srting_cols = ['class']
-for col in srting_cols:
-    dataset[col] = LabelEncoder().fit_transform(dataset[col])
-
-# Drop string columns that won't help 
-drop_cols = ['ip.src','ip.dst','tcp.flags','tcp.payload','mqtt.clientid','mqtt.msg','mqtt.topic']
-dataset = dataset.drop(columns=drop_cols) 
+dataset['class'] = LabelEncoder().fit_transform(dataset['class'])
+dataset = dataset.select_dtypes(include=['number'])
 
 print("Dataset after converting strings to numbers:")
 print(dataset.head())
@@ -25,3 +23,10 @@ y = dataset['label'] #this is what we want to predict
 
 print("Features shape:", X.shape)
 print("Labels shape:", y.shape)
+
+# split train and test
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+print("Training samples:", X_train.shape[0])
