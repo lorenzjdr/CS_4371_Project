a2979ec92d857a8b090e78878420ce4c1584ba5d
Olivia Laurel <OLaurel113@gmail.com>
2026-04-14 10:29:27 -0500

    added larger test dataset

--- anomaly_data/anomaly.py
+++ anomaly_data/anomaly.py
@@ -3,9 +3,9 @@ import random
 import os
 
 DATASET = "../Dataset/environmentMonitoring.csv" #og dataset
-OUTPUT_DATA = "anomaly_datasets5"   #anomaly log
-INTEGRITY_COUNT = 5
-AVAILABILITY_COUNT = 5
+OUTPUT_DATA = "anomaly_datasets50"   #anomaly log
+INTEGRITY_COUNT = 50
+AVAILABILITY_COUNT = 50
 
 # columns that can be corrupted
 Integrity_Columns=[
@@ -41,7 +41,7 @@ for i in rows_to_corrupt:
     log.append(["integrity_dataset.csv", "integrity", i, col, old_val, new_val])
 
 #save integrity anomaly dataset
-integrity_file = os.path.join(OUTPUT_DATA, "integrity_dataset5.csv")
+integrity_file = os.path.join(OUTPUT_DATA, "integrity_dataset50.csv")
 df_integrity.to_csv(integrity_file, index=False)
 
 #AVAILABILITY ANOMALY CREATION
@@ -53,12 +53,12 @@ for i in rows_to_delete:
     log.append(["availability_dataset.csv", "availability", i, "row_deleted", "row_exists", "row_removed"])
 
 #df_availability.drop(index=rows_to_delete, inplace=True)
-availability_file = os.path.join(OUTPUT_DATA, "availability_dataset5.csv")
+availability_file = os.path.join(OUTPUT_DATA, "availability_dataset50.csv")
 df_availability.to_csv(availability_file, index=False)
 
 #SAVING
 log_df = pd.DataFrame(log, columns=log_columns)
-log_file = os.path.join(OUTPUT_DATA, "anomaly_log5.csv")
+log_file = os.path.join(OUTPUT_DATA, "anomaly_log50.csv")
 log_df.to_csv(log_file, index=False)
 
 print("Datasets created and logged successfully!")
